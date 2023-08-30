bab24658a004898a42e4bfffe1aa513d1f44a08d
Aziz <91471349+AzizJatoi@users.noreply.github.com>
2023-08-30 20:08:49 +0500

    Added files

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 let homeScore = document.getElementById("home-score")
 
-
+let guestScore = document.getElementById("guest-score")
 
 let count = 0
+let counter = 0
 
 function addOne() {
     count = count + 1
     homeScore.textContent = count
 }
-add()
+function addTwo() {
+    count = count + 2
+    homeScore.textContent = count
+}
+function addThree() {
+    count = count + 3
+    homeScore.textContent = count
+}
+
+function guestAddOne() {
+    counter = counter + 1
+    guestScore.textContent = counter
+}
+
+function guestAddTwo() {
+    counter = counter + 2
+    guestScore.textContent = counter
+}
+function guestAddThree() {
+    counter = counter + 3
+    guestScore.textContent = counter
+}
